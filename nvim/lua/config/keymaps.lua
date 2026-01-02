f487ec4d50022146b4f0c8a9f5b11b949fded1e0
saeeeeru <saeeeeru29@gmail.com>
2026-01-03 00:49:16 +0900

    2nd
    vimade, keymaps q -> Esc

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 -- lua/keymaps.lua
-vim.keymap.set("n", "q", function()
+vim.keymap.set("n", "<Esc>", function()
   local win = vim.api.nvim_get_current_win()
   local cfg = vim.api.nvim_win_get_config(win)
   if cfg.relative ~= "" then
