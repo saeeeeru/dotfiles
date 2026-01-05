98f5aaddd17896a36fd3585f304592593af2442c
saeeeeru <saeru.yamamuro.001@layerx.co.jp>
2026-01-05 10:25:28 +0900

    telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,35 +1,36 @@
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  lazy = false,
   keys = {
     -- ファイル検索
     { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
     { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Live Grep' },
     { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Buffers' },
     { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Help Tags' },
-    
     -- 最近使ったファイル
     { '<leader>fr', '<cmd>Telescope oldfiles<cr>', desc = 'Recent Files' },
-    
     -- Git関連
     { '<leader>gc', '<cmd>Telescope git_commits<cr>', desc = 'Git Commits' },
     { '<leader>gs', '<cmd>Telescope git_status<cr>', desc = 'Git Status' },
-    
     -- LSP関連
     { '<leader>lr', '<cmd>Telescope lsp_references<cr>', desc = 'LSP References' },
     { '<leader>ld', '<cmd>Telescope lsp_definitions<cr>', desc = 'LSP Definitions' },
     { '<leader>ls', '<cmd>Telescope lsp_document_symbols<cr>', desc = 'Document Symbols' },
   },
-  opts = {
-    defaults = {
-      mappings = {
-        i = {
-          -- Ctrl+jでプレビュー下スクロール
-          ['<C-j>'] = require('telescope.actions').preview_scrolling_down,
-          -- Ctrl+kでプレビュー上スクロール
-          ['<C-k>'] = require('telescope.actions').preview_scrolling_up,
+  opts = function()
+    local actions = require('telescope.actions')
+    return {
+      defaults = {
+        mappings = {
+          i = {
+            -- Ctrl+jでプレビュー下スクロール
+            ['<C-j>'] = actions.preview_scrolling_down,
+            -- Ctrl+kでプレビュー上スクロール
+            ['<C-k>'] = actions.preview_scrolling_up,
+          },
         },
       },
-    },
-  },
+    }
+  end,
 }
