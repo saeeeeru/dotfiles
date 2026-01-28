e754be94a7bbc9393538fd65e50e0edde467424a
saeeeeru <saeru.yamamuro.001@layerx.co.jp>
2026-01-28 15:57:05 +0900

    octo -> snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,12 +2,21 @@ return {
 "folke/snacks.nvim",
 version = "*",
 lazy = false, -- for dashboard
+keys = {
+  { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues" },
+  { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub PRs" },
+  { "<leader>gia", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (All)" },
+  { "<leader>gpa", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub PRs (All)" },
+},
 ---@type snacks.Config
 opts = {
 scratch = { enable = true },
 notifier = {
   enabled = true,
   timeout = 3000, -- default timeout in ms
+},
+gh = {
+  enabled = true,
 },
 styles = {
 scratch = {
